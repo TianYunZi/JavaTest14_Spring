d9d64d7dade43f5510444996ea8ebb499ba647ac
TianYunZi <zxysegh@gmail.com>
2018-03-15 21:07:37 +0800

    spring security config

---
+++ 6.1.1.spring-security-config/src/main/java/org/spring/security/practise/Test.java
@@ -0,0 +1,4 @@
+package org.spring.security.practise;
+
+public class Test {
+}
---
+++ 6.1.1.spring-security-config/src/main/java/org/spring/security/practise/config/SecurityWebInitializer.java
@@ -0,0 +1,6 @@
+package org.spring.security.practise.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+}
